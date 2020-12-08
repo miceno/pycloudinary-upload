4401307026836035a328cd81d1a8fd21b3baba1c
Orestes Sanchez <orestes.sanchezbenavente@telefonica.com>
2020-12-08 19:54:45 +0100

    Allow concurrent processing for faster uploads.

--- README.md
+++ README.md
@@ -3,6 +3,15 @@ Cloudinary upload tree
 
 This is a python script to upload a folder and its subfolders to Cloudinary CDN.
 
+Transformation quota
+===
+
+Every image or video upload counts on your Transformation quota, since Cloudinary
+analyses the image and prepares it for the CDN.
+
+Use `raw` resource_type so image uploads will not count on your Transformation quota. 
+See https://cloudinary.com/blog/understanding_cloudinary_s_transformation_quotas 
+
 Setup
 ===
 
@@ -16,14 +25,18 @@ Usage
 
 ```commandline
 
-usage: upload_tree [-h] 
-                --cloud_name CLOUD_NAME 
-                --api_key API_KEY 
-                --api_secret API_SECRET 
-                [--destination-folder DESTINATION_FOLDER]
-                [--exclude-files EXCLUDE_FILES]
-                [--resource-type RESOURCE_TYPE]
-                base_folder
+usage: pycloud-upload [-h] 
+    --cloud_name CLOUD_NAME 
+    --api_key API_KEY 
+    --api_secret API_SECRET
+    [--destination-folder DESTINATION_FOLDER] 
+    [--unique-filename] [--no-unique-filename] 
+    [--use-filename] [--no-use-filename] 
+    [--exclude-files EXCLUDE_FILES] 
+    [--tag TAGS] 
+    [--resource-type {image,raw,video,auto}]
+    [--concurrent_workers CONCURRENT_WORKERS]
+    base_folder
 
 Upload a tree folder to Cloudinary
 
@@ -40,7 +53,55 @@ optional arguments:
                         Cloudinary API secret (default: None)
   --destination-folder DESTINATION_FOLDER, -d DESTINATION_FOLDER
                         Destination base folder (default: None)
+  --unique-filename     When set to true, add random characters at the end of the filename that guarantee its uniqueness. This
+                        parameter is relevant only if use_filename is also set to true. (default: False)
+  --no-unique-filename  When set to false, does not add random characters at the end of the filename that guarantee its uniqueness.
+                        This parameter is relevant only if use_filename is also set to true. (default: False)
+  --use-filename        When true, the uploaded file's original filename becomes the Public ID. Random characters are appended to the
+                        filename value to ensure Public ID uniqueness if unique_filename is true. (default: True)
+  --no-use-filename     When false, the Public ID will be comprised of random characters. (default: True)
   --exclude-files EXCLUDE_FILES, -x EXCLUDE_FILES
                         Exclude files (default: [])
+  --tag TAGS, -t TAGS   Set tags on uploaded files (default: [])
+  --resource-type {image,raw,video,auto}, -r {image,raw,video,auto}
+                        Set resource type. raw means no transformation on upload (default: auto)
+  --concurrent_workers CONCURRENT_WORKERS, -w CONCURRENT_WORKERS
+                        Specify number of concurrent network threads. (default: 10)
+
+
+```
+
+
+Example:
+```commandline
+
+python pycloud-upload.py
+        -r raw                
+        -c ahpn 
+        -a API_KEY 
+        -s SECRET
+        --destination-folder CLOUDINARY_DESTINATION_FOLDER 
+        SOURCE_FOLDER
+
+python pycloud-upload.py
+    -c ahpn -a API_KEY -s SECRET_KEY
+    -r raw 
+    --destination-folder tiles/poblenou-1871/20/530565 
+    ~/Downloads/1871/1871/20/530565
+
+```
+
+# Cloudinary CLI
+
+Have a look at it https://cloudinary.com/documentation/cloudinary_cli
+
+## Upload directory
+
+It does not allow to set `raw` resource_type so every upload will count for your
+Transformation quota.
+
+```commandline
+
+cld -C ahpn upload_dir -f "tiles/poblenou-1871" ~/Downloads/1871/1871/20/530564
 
 ```
--- pycloud-upload.py
+++ pycloud-upload.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from multiprocessing import pool
 
 import cloudinary
 from cloudinary.uploader import upload
@@ -25,6 +26,33 @@ def parse_args(app_name, description=None):
     parser.add_argument("base_folder", action="store", help="Base folder to upload")
     parser.add_argument("--destination-folder", '-d', dest='destination_folder', action="store", default=None,
                         help="Destination base folder")
+
+    parser.add_argument('--unique-filename', dest='unique_filename', action='store_true',
+                        help="""
+                        When set to true, add random characters at the end of
+                        the filename that guarantee its uniqueness.
+                        This parameter is relevant only if use_filename is also set to true.
+                        """)
+    parser.add_argument('--no-unique-filename', dest='unique_filename', action='store_false',
+                        help="""
+                        When set to false, does not add random characters at the end of
+                        the filename that guarantee its uniqueness.
+                        This parameter is relevant only if use_filename is also set to true.
+                        """)
+    parser.set_defaults(unique_filename=False)
+
+    parser.add_argument('--use-filename', dest='use_filename', action='store_true',
+                        help="""
+                        When true, the uploaded file's original filename becomes the Public ID. 
+                        Random characters are appended to the filename value to ensure Public ID uniqueness 
+                        if unique_filename is true.
+                        """)
+    parser.add_argument('--no-use-filename', dest='use_filename', action='store_false',
+                        help="""
+                        When false, the Public ID will be comprised of random characters.
+                        """)
+    parser.set_defaults(use_filename=True)
+
     parser.add_argument("--exclude-files", '-x', dest='exclude_files', action="append", default=[],
                         help="Exclude files")
     parser.add_argument("--tag", '-t', dest='tags', action="append", default=[],
@@ -33,6 +61,10 @@ def parse_args(app_name, description=None):
                         choices=['image', 'raw', 'video', 'auto'],
                         default='auto',
                         help="Set resource type. raw means no transformation on upload")
+    parser.add_argument("--concurrent_workers", "-w",
+                        type=int,
+                        default=10,
+                        help="Specify number of concurrent network threads.")
 
     arguments = parser.parse_args()
 
@@ -60,8 +92,28 @@ def upload_file(source_filename, destination_filename, base_folder, tags=[], res
     result = upload(source_filename, folder=os.path.join(base_folder, os.path.dirname(destination_filename)),
                     use_filename=True, unique_filename=False, tags=tags, resource_type=resource_type)
 
+    return result
+
+
+def upload_file_concurrent(source_filename,
+                           destination_filename,
+                           base_folder,
+                           options):
+    """
+    Upload a file to base folder.
+
+    :param source_filename:
+    :param destination_filename:
+    :param base_folder:
+    :param options:
+    :return:
+    """
+    print('upload_file', base_folder, source_filename, destination_filename, options)
+    result = upload(source_filename,
+                    folder=os.path.join(base_folder, os.path.dirname(destination_filename)),
+                    **options)
     print(result)
-    pass
+    return result
 
 
 def cloudinary_init(cloud_name="sample", api_key="874837483274837", api_secret="a676b67565c6767a6767d6767f676fe1"):
@@ -75,6 +127,7 @@ def cloudinary_init(cloud_name="sample", api_key="874837483274837", api_secret="
 def create_destination_folder(folder):
     """
     Create a remote folder
+    This is a noop, since target folders are created on the fly.
 
     :param folder:
     :return:
@@ -107,13 +160,16 @@ def upload_tree(base_folder, exclude_files=EXCLUDE_FILES, destination_base_folde
         destination_folder = destination_base_folder
         if relative_path != '.':
             destination_folder = os.path.join(destination_base_folder, relative_path)
+        results = []
         for filename in files:
             print(subdir, filename)
             if filename in exclude_files:
                 print("Ignoring file", filename)
                 continue
-            upload_file(os.path.join(subdir, filename), filename, destination_folder, tags, resource_type)
+            result = upload_file(os.path.join(subdir, filename), filename, destination_folder, tags, resource_type)
+            results.append(result)
 
+        print(results, len(results))
         # Create every subfolder
         for d in dirs:
             if d in exclude_files:
@@ -124,9 +180,65 @@ def upload_tree(base_folder, exclude_files=EXCLUDE_FILES, destination_base_folde
         print("*" * 10)
 
 
+def run_tasks_concurrently(func, tasks, concurrent_workers):
+    thread_pool = pool.ThreadPool(concurrent_workers)
+    thread_pool.starmap(func, tasks)
+
+
+def upload_tree_concurrent(base_folder,
+                           exclude_files=EXCLUDE_FILES,
+                           destination_base_folder='.',
+                           concurrent_workers=10,
+                           **options):
+    """
+    Upload a folder with structure
+
+    :param base_folder:
+    :param exclude_files:
+    :param destination_base_folder:
+    :param concurrent_workers:
+    :param options:
+    :return:
+    """
+
+    uploads = []
+    for subdir, dirs, files in os.walk(base_folder):
+
+        print("Processing", subdir)
+        if os.path.basename(subdir) in exclude_files:
+            print("Ignoring subdir", subdir)
+            continue
+        # Upload files
+        relative_path = os.path.relpath(subdir, base_folder)
+
+        destination_folder = destination_base_folder
+        if relative_path != '.':
+            destination_folder = os.path.join(destination_base_folder, relative_path)
+        for filename in files:
+            print(subdir, filename)
+            if filename in exclude_files:
+                print("Ignoring file", filename)
+                continue
+            params = (os.path.join(subdir, filename), filename, destination_folder, options)
+            uploads.append(params)
+
+        # Create every target subfolder
+        for d in dirs:
+            if d in exclude_files:
+                print("Ignoring folder", d)
+                continue
+            create_destination_folder(os.path.join(destination_folder, d))
+
+    run_tasks_concurrently(upload_file_concurrent, uploads, concurrent_workers)
+
+
 if __name__ == "__main__":
-    args = parse_args("upload_tree", "Upload a tree folder to Cloudinary")
+    args = parse_args("pycloud-upload", "Upload a tree folder to Cloudinary")
     print(args)
 
     cloudinary_init(args.cloud_name, args.api_key, args.api_secret)
-    upload_tree(args.base_folder, args.exclude_files, args.destination_folder, args.tags, args.resource_type)
+    upload_tree_concurrent(args.base_folder, exclude_files=args.exclude_files,
+                           destination_base_folder=args.destination_folder, tags=args.tags,
+                           resource_type=args.resource_type,
+                           use_filename=args.use_filename,
+                           unique_filename=args.unique_filename)
